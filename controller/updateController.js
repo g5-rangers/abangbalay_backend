6ad6ee0adabe5232a2832973cea3f4f24b8483a8
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-12-09 20:43:51 +0800

    ok

--- controller/updateController.js
+++ controller/updateController.js
@@ -2,10 +2,10 @@ const User = require('../model/userModel');
 
 exports.updateAccount = (req, res) => {
     console.log(req.body)
-    const test = async function() {
+    const test = async function () {
         const update = await User.updateUser(req.body.email, req.body.newemail, req.body.newpassword)
         console.log(await User.getByEmail(req.body.newemail))
         res.send(await User.getByEmail(req.body.newemail))
-    }    
+    }
     test()
 }
